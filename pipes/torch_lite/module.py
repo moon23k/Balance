ce09b07bf165c39c851bafa8f66b98fe425dbf20
moon <d.moon23k@gmail.com>
2023-04-27 21:33:39 +0900

    code updates

--- pipes/torch_lite/module.py
+++ pipes/torch_lite/module.py
@@ -1,54 +1,110 @@
-import torch
+import json, torch, evaluate
 from torch import nn
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from torch.nn import functional as F
+from torch.nn.utils.rnn import pad_sequence
 
+
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, split):
+        super().__init__()
+        self.data = self.load_data(split)
+
+    @staticmethod
+    def load_data(split):
+        with open(f"data/{split}.json", 'r') as f:
+            data = json.load(f)
+        return data
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        input_ids = self.data[idx]['input_ids']
+        labels = self.data[idx]['labels']
+        
+        return input_ids, labels
 
 
 
 class PLModule(pl.LightningModule):
-	def __init__(self, config):
+	def __init__(self, config, model, tokenizer=None):
 		super(PLModule, self).__init__()
   
         self.lr = config.lr
+        self.pad_id = config.pad_id
         self.device = config.device
-		self.model = T5ForConditionalGeneration.from_pretrained(config.mname).to(self.device)
+		self.batch_size = config.batch_size
+        
+        self.model = model
+        if self.mode == 'test':
+            self.tokenizer = tokenizer
+            self.metric_module = evaluate.load('bleu')
+
+
+    def _collate_fn(self, batch):
+        ids_batch, labels_batch = [], []
+        for ids, labels in batch:
+            ids_batch.append(ids) 
+            labels_batch.append(labels)
+
+        return {'input_ids': pad_sequence(ids_batch, batch_first=True, padding_value=self.pad_id),
+                'labels': pad_sequence(labels_batch, batch_first=True, padding_value=self.pad_id)}
+
+
+    def train_dataloader(self):
+        return DataLoader(Dataset('train'), 
+                          batch_size=self.batch_size,
+                          collate_fn=self._collate_fn,
+                          shuffle=True, 
+                          pin_memory=True,
+                          num_workers=2)
 
 
     def training_step(self, batch, batch_idx):
-        input_ids = batch['input_ids'].to(self.device), 
-        attention_mask = (input_ids == self.pad_id).to(self.device)
-        labels = batch['labels'].to(self.device)      
-
-        loss = self.model(input_ids=input_ids, 
-                          attention_mask=attention_mask,
-                          labels=labels).loss
+        loss =  self.model(input_ids=batch['input_ids'].to(self.device), 
+                           attention_mask=(input_ids == self.pad_id).to(self.device),
+                           labels=batch['labels'].to(self.device)).loss
+        
+        self.log('train loss', loss)
+        self.log('train ppl', math.exp(loss))
         
         return loss
 
     def validataion_step(self, batch, batch_idx):
-        input_ids = batch['input_ids'].to(self.device), 
-        attention_mask = (input_ids == self.pad_id).to(self.device)
-        labels = batch['labels'].to(self.device)      
+        loss =  self.model(input_ids=batch['input_ids'].to(self.device), 
+                           attention_mask=(input_ids == self.pad_id).to(self.device),
+                           labels=batch['labels'].to(self.device)).loss
         
-        loss = self.model(input_ids=input_ids, 
-                          attention_mask=attention_mask,
-                          labels=labels).loss
+        self.log('train loss', loss)
+        self.log('train ppl', math.exp(loss))
         
-        return loss
+
 
     def test_step(self, batch, batch_idx):
-        input_ids = batch['input_ids'].to(self.device), 
+        input_ids = batch['input_ids'].to(self.device)
         attention_mask = (input_ids == self.pad_id).to(self.device)
-        labels = batch['labels'].to(self.device)      
+
+        greedy_pred = self.model.generate(input_ids=input_ids, 
+                                          attention_mask=attention_mask, 
+                                          use_cache=True)
         
-        loss = self.model(input_ids=input_ids, 
-                          attention_mask=attention_mask,
-                          labels=labels).loss
+        beam_pred = self.model.generate(input_ids=input_ids, 
+                                        attention_mask=attention_mask, 
+                                        use_cache=True)
         
-        return loss
+        greedy_pred = self.tokenizer.batch_decode(greedy_pred)
+        beam_pred = self.tokenizer.batch_decode(beam_pred)
+        labels = self.tokenizer.batch_decode(labels)
+
+        greedy_score = self.metric_module.compute(greedy_pred, labels)
+        beam_score = self.metric_module.compute(beam_pred, labels)
+
+        return {'greedy_score': greedy_score,
+                'beam_score': beam_score}
 
 
     def configure_optimizers(self):
-        return optim.AdamW(self.model.parameters(), self.lr)                   
+        return optim.AdamW(self.model.parameters(), self.lr)
---
+++ pipes/torch_lite/run.py
@@ -0,0 +1,135 @@
+import os, argparse, torch
+from module import PLModule
+from transformers import (set_seed, 
+	 					  T5Config,
+	 					  T5ForConditionalGeneration)
+
+
+
+class Config(object):
+    def __init__(self, mode):    
+
+        self.mode = mode
+        self.mname = 't5-base'
+        self.ckpt = f"ckpt/{self.pipe}.pt"
+        
+        self.clip = 1
+        self.lr = 5e-5
+        self.max_len = 300
+        self.n_epochs = 10
+        self.batch_size = 128
+        self.iters_to_accumulate = 4
+
+        self.early_stop = True
+        self.patience = 3
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def print_attr(self):
+        for attribute, value in self.__dict__.items():
+            print(f"* {attribute}: {value}")
+
+
+
+def load_model(config):
+
+    #Inner methods
+    def count_params(model):
+        params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        return params
+        
+    def check_size(model):
+        param_size, buffer_size = 0, 0
+
+        for param in model.parameters():
+            param_size += param.nelement() * param.element_size()
+        
+        for buffer in model.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+
+        size_all_mb = (param_size + buffer_size) / 1024**2
+        return size_all_mb
+
+
+    #Actual Process
+    if config.mode == 'train':
+        model = T5ForConditionalGeneration.from_pretrained(config.m_name)
+        print("Pretrained T5 Model has loaded")
+    
+
+    elif config.mode != 'train':
+        assert os.path.exists(config.ckpt)
+        
+        model_config = T5Config.from_pretrained(config.m_name)
+        model = T5ForConditionalGeneration(model_config)
+        print("Initialized T5 Model has loaded")
+        
+        model_state = torch.load(config.ckpt, map_location=config.device)['model_state_dict']
+        model.load_state_dict(model_state)
+        print(f"Trained Model states has loaded from {config.ckpt}")
+
+
+    print(f"--- Model Params: {count_params(model):,}")
+    print(f"--- Model  Size : {check_size(model):.3f} MB\n")
+    
+    return model.to(config.device)
+
+
+
+def inference(model, tokenizer):
+    model.eval()
+    print(f'--- Inference Process Started! ---')
+    print('[ Type "quit" on user input to stop the Process ]')
+    
+    while True:
+        input_seq = input('\nUser Input Sequence >> ').lower()
+
+        #End Condition
+        if input_seq == 'quit':
+            print('\n--- Inference Process has terminated! ---')
+            break        
+
+        #convert user input_seq into model input_ids
+        encodings = tokenizer(input_seq)
+
+        if isinstance(model, EncoderDecoderModel):
+            preds = model.generate(**encodings, use_cache=True)
+        else:
+            preds = model.generate(**encodings)
+
+        preds = tokenizer.decode(preds, skip_special_tokens=True)
+
+        #Search Output Sequence
+        print(f"Model Out Sequence >> {preds}")       
+
+
+
+def main(mode):
+	set_seed(42)
+	config = Config(mode)
+	model = load_model(config)
+	tokenizer = T5TokenizerFast.from_pretrained(config.mname)
+    setattr(config, 'vocab_size', tokenizer.vocab_size)
+    setattr(config, 'pad_id', tokenizer.pad_token_id)
+
+	
+	if mode == 'train':
+        module = PLModule(config, model)
+        module.train()
+
+	elif mode == 'test':
+		module = PLModule(config, model, tokenizer)
+        module.test()
+
+	elif model == 'inference':
+		inference(config, model)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-mode', required=True)
+    args = parser.parse_args()
+    assert args.mode in ['train', 'test', 'inference']    
+
+    main(args.mode)
